fb25327d69431e6c650bbd48a3ca51275ab8470d
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-25 17:23:28 +0530

    Login + Landing + raise Ticket page added

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///itsm_ticket_resolution.db"
+app.config['SQLALCHEMY_BINDS'] = {    
+    'ticket_db': 'sqlite:///ticket.db'
+}
+db=SQLAlchemy(app)
+class User(db.Model):
+    username=db.Column(db.String(200),primary_key=True)
+    password=db.Column(db.String(200),nullable=False)
+
+class Ticket(db.Model):
+    TicketNumber=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    shortDesc=db.Column(db.String(200),nullable=False)
+    longDesc=db.Column(db.String(200))
+    IssueType=db.Column(db.String(10))
+    Priority=db.Column(db.String(10))
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/login')
+def login_page():
+    #now we need to add the user to our database (used SQLLite)
+    user=User(username="abc@gmail.com",password="priya@123")
+    db.session.add(user)
+    db.session.commit()
+    return render_template('login.html')
+
+#creating landing page endpoint
+@app.route('/welcome')
+def welcome_page():
+    return render_template('welcome.html')
+
+@app.route('/raise_ticket',methods=['GET','POST'])
+def raise_ticket():    
+    if(request.method=='POST'):
+        __bind_key__ = 'ticket_db'
+        shortDesc=request.form['shortDesc']
+        longDesc=request.form['longDesc']
+        print(shortDesc)
+        print(longDesc)  
+        ticket=Ticket(shortDesc=shortDesc,longDesc=longDesc)
+        print(ticket)
+        db.session.add(ticket)
+        db.session.commit()
+        print("Count: ",Ticket.query.count())
+        for t in Ticket.query.all():
+            print(t.shortDesc)
+    return render_template('raise_ticket.html')
+
+with app.app_context():
+    db.create_all()
+
+if __name__=="__main__":
+    app.run(debug=True,port=5001)
