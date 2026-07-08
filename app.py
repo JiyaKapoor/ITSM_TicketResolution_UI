ba9a000d849a685939699ad1f03acab3d29d6569
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-07-08 19:54:03 +0530

    Ticket query added

--- app.py
+++ app.py
@@ -179,6 +179,7 @@ def raise_ticket():
 @app.route('/accept_resolution')
 def accept_rag_resolution():
     ticket_number=request.args.get("ticket_number")
+    ticket=Ticket.query.filter_by(Ticket.TicketNumber==ticket_number).first()
 
 @app.route('/my_tickets')
 def fetch_my_tickets():
